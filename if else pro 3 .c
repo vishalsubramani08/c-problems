3f1fdad80da17c0cb7335ff549270a2e22723043
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-02-11 13:39:56 +0530

    Add files via upload

---
+++ if else pro 3 .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if (a%5==0){
+    printf("divisible by 5");
+    }
+    else  {
+    printf("not divisible by 5");
+    }
+    
+    return 0;
+}
