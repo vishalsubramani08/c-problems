5d3c4b51f492b0065a16a7059b5e8d4eafb28086
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-02-12 15:34:32 +0530

    Add files via upload

---
+++ if else pro 23.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+int a;
+scanf("%d",&a);
+switch(a){
+    case 1:
+    printf("31 days");
+    break;
+    case 2:
+    printf("28 or 29 days");
+    break;
+    case 3:
+    printf("31 days");
+    break;
+    case 4:
+    printf("30 days");
+    break;
+    case 5:
+    printf("31 days");
+    break;
+    case 6:
+    printf("30 days");
+    break;
+    case 7:
+    printf("31 days");
+    break;
+   case 8:
+   printf("31 days");
+   break;
+   case 9:
+   printf("30 days");
+   break;
+   case 10:
+   printf("31 days");
+   break;
+   case 11:
+   printf("30 days");
+   break;
+   case 12:
+  printf("31 days");
+  break;
+default:
+printf("invalid month");
+return 0;
+}}
