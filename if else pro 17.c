ae0221690b668bd83b429db5344b07b69e3c397d
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-02-12 10:29:21 +0530

    Add files via upload

---
+++ if else pro 17.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int a;
+scanf ("%d",&a);
+if(a>10&& a<20){
+printf("number with in range");
+}
+
+else {
+    printf(" number without in range");
+}
+    return 0;
+}
