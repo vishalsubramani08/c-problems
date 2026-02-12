54190ed1c3d153e38729c332431ff63af1d72b9a
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-02-12 10:23:36 +0530

    Add files via upload

---
+++ if else pro 16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int a,b ,c;
+scanf ("%d%d%d",&a,&b,&c);
+if(a==b&& a==c){
+printf("equilateral triangle");
+}
+
+else {
+    printf(" not equilateral triangle");
+}
+    return 0;
+}
