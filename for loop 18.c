dd73dda3a46393d9b0ef9a0db6d011a1bacd1522
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-02-17 12:10:24 +0530

    Add files via upload

---
+++ for loop 18.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a,b ,max=0;
+    scanf("%d%d",&a,&b);
+    if(a<b){
+        for(int i=1;i<=a;i++){
+            if(a%i==0 && b%i==0)            max=i;
+        }
+        
+    }
+    
+    else{
+        for(int i=1;i<=b;i++){
+            if(a%i==0  && b%i==0)            max=i;
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
+    
