5e51309e830f20edb7888dcf4fcec4f1849c4ad6
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-02-19 10:33:40 +0530

    Add files via upload

---
+++ for loop 2.3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+     int c=0,a,b ;
+     
+     scanf("%d",&a);
+  for( int i =0;i<a;i++){
+    scanf("%d",&b);
+    c=c+b;
+  }
+ printf("total units:%d",c);   
+
+return 0;
+}
+
+
