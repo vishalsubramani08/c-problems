604136a6ed45acfdee7bae3059de6c6ecd532eec
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-03-02 17:20:29 +0530

    Add files via upload

---
+++ n 4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+    int i,j;
+    int ch='A';
+    for ( int i=1;i<=5;i++){
+    for ( int j=1;j<=i;j++){
+    printf("%c",ch);
+    }
+    ch++;
+    printf ("\n");
+    }
+    return 0;
+}
---
+++ n 6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main (){
+    int  a,b=1;
+    scanf("%d",&a);
+    for ( int i=1;i<=5;i++){
+    for ( int j=1;j<=i;j++){
+        n++;
+        if(b%2==0){
+            printf("1");
+        }
+        else{
+    printf("0");
+    }
+    }
+        printf ("\n");
+    }
+    return 0;
+}
