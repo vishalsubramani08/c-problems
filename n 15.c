c571fe83a30060812fc2e248300e78cbb88b941b
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-03-02 17:31:17 +0530

    Create n 15.c

---
+++ n 15.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++){
+        
+        for(int j=a;j>=1;j--){
+            if(i>=j)
+            printf("* ");
+            else
+            printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
