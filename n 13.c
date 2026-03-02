b5e7458b95d2deeca6909b7c703b60da215eab06
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-03-02 17:30:05 +0530

    Create n 13.c

---
+++ n 13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    for(int i=0;i<a;i++){
+        
+        for(int j=0;j<a;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
