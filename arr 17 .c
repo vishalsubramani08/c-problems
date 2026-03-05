38257fdd56aecb07f86b444926fcb47b0383a1a3
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-03-05 14:39:38 +0530

    Create arr 17 .c

---
+++ arr 17 .c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for (int i=0;i<a;i++){
+        scanf("%d",&arr[i]);
+    }
+    int b=1; 
+    for (int i=0;i<a/2;i++) {
+        if (arr[i]!=arr[a-i-1]){
+            b=0; 
+            break;
+        }
+    }
+    if (b)
+        printf("Yes\n");
+    else
+        printf("No");
+    return 0;
+}
